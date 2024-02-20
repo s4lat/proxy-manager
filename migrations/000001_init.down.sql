1a640cbb7bbaae0484951bb34dff12258bcace5c
s4lat <zakazchikm@gmail.com>
2024-02-20 22:02:27 +0300

    [codeDrop] migrations;

---
+++ migrations/000001_init.down.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS proxy_occupy;
+DROP TABLE IF EXISTS proxy;
---
+++ migrations/000001_init.up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS proxy
+(
+    proxy_id        BIGSERIAL PRIMARY KEY,
+    protocol        VARCHAR(32),
+    username        VARCHAR(255),
+    password        VARCHAR(255),
+    host            VARCHAR(255),
+    port            BIGINT,
+    expiration_date timestamp
+);
+
+CREATE TABLE IF NOT EXISTS proxy_occupy
+(
+    key              UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    proxy_id         BIGINT REFERENCES proxy (proxy_id) ON DELETE CASCADE,
+    create_timestamp DOUBLE PRECISION
+);
